3ce5ea6568e363c1a2408f406f702a81638e558d
Christian Lung <christianlung@s-149-142-34-13.resnet.ucla.edu>
2023-04-17 14:26:36 -0700

    Print argc and argv

--- pipe.c
+++ pipe.c
@@ -1,4 +1,11 @@
+#include <stdio.h>
+
 int main(int argc, char *argv[])
 {
+	printf("There are %d arguemnts\n", argc);
+	printf("They are: \n");
+	for(int i=0; i<argc; i++){
+		printf("argv %d is: %s \n", i, argv[i]);
+	}
 	return 0;
 }
