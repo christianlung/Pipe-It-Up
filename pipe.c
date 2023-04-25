02e8eb6b47bc989e560367a82a1778cb366b800c
Christian Lung <christianlung@s-149-142-34-41.resnet.ucla.edu>
2023-04-25 00:50:12 -0700

    Successful ./pipe with 2 arguments

--- pipe.c
+++ pipe.c
@@ -7,27 +7,37 @@
 
 int main(int argc, char *argv[])
 {
-	// printf("I am lazy, letting other program to ls the directory\n\n");
-	// printf("I will just wait for their report \n\n");
-	// execlp(argv[1], argv[1], NULL);
-	// printf("They Finished; Done!\n");
-	int return_code = fork();
-	if(return_code == 0){
-		printf("This is the child process \n");
+	int fds[2];
+	//fds[0] is the read end
+	//fds[1] is the write end
+	if(pipe(fds)==-1){
+		return 1;
+	}
+	int return_code1 = fork();
+	if(return_code1<0) printf("Child process 1 creation error\n");
+	if(return_code1 == 0){
+		dup2(fds[1], STDOUT_FILENO);
+		close(fds[1]);
+		close(fds[0]);
 		execlp(argv[1],argv[1],NULL);
-		exit(0);
 	}
-	else if(return_code>0){
-		printf("i am lazy parent, letting my child execute a command\n");
-		printf("I will wait\n");
-		int pid = return_code;
-		int status = 0;
-		waitpid(pid, &status, 0);
-		printf("Child process exits with code: %d \n", WEXITSTATUS(status));
+
+	int return_code2 = fork();
+	if(return_code2<0) printf("Child process 2 creation error\n");
+	if(return_code2==0){
+		dup2(fds[0], STDIN_FILENO);
+		close(fds[0]);
+		close(fds[1]);
+		execlp(argv[2],argv[2],NULL);
 	}
-	else{
-		printf("child process creation error\n");
-	}
-	printf("They finished; Done!\n");
+
+	close(fds[0]);
+	close(fds[1]);
+
+	int status1, status2 = 0;
+	waitpid(return_code1, &status1, 0);
+	waitpid(return_code2, &status2, 0);
+	// printf("Child process 1 extis with code: %d \n", WEXITSTATUS(status1));
+	// printf("Child process 2 exits with code: %d \n", WEXITSTATUS(status2));
 	return 0;
 }
