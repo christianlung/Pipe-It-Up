0214224e078387ad2ce38a5a581b8f1667bc65a2
Christian Lung <christianlung@s-149-142-34-41.resnet.ucla.edu>
2023-04-25 01:57:42 -0700

    Succesfully implemented 3 argument pipe operator

--- pipe.c
+++ pipe.c
@@ -5,39 +5,53 @@
 #include <fcntl.h>
 #include <errno.h>
 
+//route standard errors into file
+//add error checkers like WSTATUS
 int main(int argc, char *argv[])
 {
-	int fds[2];
-	//fds[0] is the read end
-	//fds[1] is the write end
-	if(pipe(fds)==-1){
+	int fd[2];
+	//fd[0] is the read end
+	//fd[1] is the write end
+	if(pipe(fd)==-1){
 		return 1;
 	}
 	int return_code1 = fork();
 	if(return_code1<0) printf("Child process 1 creation error\n");
 	if(return_code1 == 0){
-		dup2(fds[1], STDOUT_FILENO);
-		close(fds[1]);
-		close(fds[0]);
+		dup2(fd[1], STDOUT_FILENO);
+		close(fd[1]);
+		close(fd[0]);
 		execlp(argv[1],argv[1],NULL);
 	}
+	close(fd[1]);
 
+	int fd1[2];
+	if(pipe(fd1)==-1){
+		return 1;
+	}
 	int return_code2 = fork();
 	if(return_code2<0) printf("Child process 2 creation error\n");
 	if(return_code2==0){
-		dup2(fds[0], STDIN_FILENO);
-		close(fds[0]);
-		close(fds[1]);
+		dup2(fd[0], STDIN_FILENO);
+		close(fd[0]);
+		dup2(fd1[1], STDOUT_FILENO);
+		close(fd1[1]);
+		close(fd1[0]);
 		execlp(argv[2],argv[2],NULL);
 	}
+	close(fd1[1]);
 
-	close(fds[0]);
-	close(fds[1]);
-
-	int status1, status2 = 0;
+	int return_code3 = fork();
+	if(return_code3<0) printf("Child process 3 creation error\n");
+	if(return_code3==0){
+		dup2(fd1[0], STDIN_FILENO);
+		close(fd1[0]);
+		execlp(argv[3],argv[3],NULL);
+	}
+	close(fd1[0]);
+	int status1, status2, status3 = 0;
 	waitpid(return_code1, &status1, 0);
 	waitpid(return_code2, &status2, 0);
-	// printf("Child process 1 extis with code: %d \n", WEXITSTATUS(status1));
-	// printf("Child process 2 exits with code: %d \n", WEXITSTATUS(status2));
+	waitpid(return_code3, &status3, 0);
 	return 0;
 }
