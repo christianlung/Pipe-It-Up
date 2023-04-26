ee3b530a20d15b3a164675d09a683b44df45b99d
Christian Lung <christianlung@Christians-MacBook-Air.local>
2023-04-26 01:38:37 -0700

    Added fourth argument/child

--- pipe.c
+++ pipe.c
@@ -13,6 +13,8 @@ int main(int argc, char *argv[])
 		execlp(argv[1],argv[1],NULL);
 		exit(1);
 	}
+
+	int status1, status2, status3, status4 = 0;
 	int fd[2];
 	//fd[0] is the read end
 	//fd[1] is the write end
@@ -27,6 +29,9 @@ int main(int argc, char *argv[])
 	}
 	close(fd[1]);
 
+	waitpid(return_code1, &status1, 0);
+	// printf("Child process 1 exits with code: %d\n", WEXITSTATUS(status1));
+
 	int fd1[2];
 	if(pipe(fd1)==-1) return 1;
 	int return_code2 = fork();
@@ -34,27 +39,44 @@ int main(int argc, char *argv[])
 	if(return_code2==0){
 		if(dup2(fd[0], STDIN_FILENO)<0) exit(1);
 		close(fd[0]);
-		dup2(fd1[1], STDOUT_FILENO);
+		if(dup2(fd1[1], STDOUT_FILENO)<0) exit(1);
 		close(fd1[1]);
 		close(fd1[0]);
 		execlp(argv[2],argv[2],NULL);
 	}
+	close(fd[0]);
 	close(fd1[1]);
 
+	waitpid(return_code2, &status2, 0);
+	// printf("Child process 2 exits with code: %d\n", WEXITSTATUS(status2));
+
+	int fd2[2];
+	if(pipe(fd2)==-1) return 1;
 	int return_code3 = fork();
 	if(return_code3<0) exit(1);
 	if(return_code3==0){
 		if(dup2(fd1[0], STDIN_FILENO)<0) exit(1);
 		close(fd1[0]);
+		if(dup2(fd2[1],STDOUT_FILENO)<0) exit(1);
+		close(fd2[1]);
+		close(fd2[0]);
 		execlp(argv[3],argv[3],NULL);
 	}
 	close(fd1[0]);
-	int status1, status2, status3 = 0;
-	waitpid(return_code1, &status1, 0);
-	// printf("Child process 1 exits with code: %d\n", WEXITSTATUS(status1));
-	waitpid(return_code2, &status2, 0);
-	// printf("Child process 2 exits with code: %d\n", WEXITSTATUS(status2));
+	close(fd2[1]);
+
 	waitpid(return_code3, &status3, 0);
 	// printf("Child process 3 exits with code: %d\n", WEXITSTATUS(status3));
+
+	int return_code4 = fork();
+	if(return_code4<0) exit(1);
+	if(return_code4==0){
+		if(dup2(fd2[0], STDIN_FILENO)<0) exit(1);
+		close(fd2[0]);
+		execlp(argv[4],argv[4],NULL);
+	}
+	close(fd2[0]);
+
+	waitpid(return_code4, &status4, 0);
 	return 0;
 }
