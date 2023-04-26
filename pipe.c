25dd0394e07375f8c2ed7a261b5b3113a4555800
Christian Lung <christianlung@Christians-MacBook-Air.local>
2023-04-25 22:04:38 -0700

    Added error checks for functions and # of arguments

--- pipe.c
+++ pipe.c
@@ -6,19 +6,21 @@
 #include <errno.h>
 
 //route standard errors into file
-//add error checkers like WSTATUS
 int main(int argc, char *argv[])
 {
+	if(argc==0) exit(EINVAL);
+	if(argc==1){
+		execlp(argv[1],argv[1],NULL);
+		exit(1);
+	}
 	int fd[2];
 	//fd[0] is the read end
 	//fd[1] is the write end
-	if(pipe(fd)==-1){
-		return 1;
-	}
+	if(pipe(fd)==-1) return 1;
 	int return_code1 = fork();
-	if(return_code1<0) printf("Child process 1 creation error\n");
+	if(return_code1<0) exit(1);
 	if(return_code1 == 0){
-		dup2(fd[1], STDOUT_FILENO);
+		if(dup2(fd[1], STDOUT_FILENO)<0) exit(1);
 		close(fd[1]);
 		close(fd[0]);
 		execlp(argv[1],argv[1],NULL);
@@ -26,13 +28,11 @@ int main(int argc, char *argv[])
 	close(fd[1]);
 
 	int fd1[2];
-	if(pipe(fd1)==-1){
-		return 1;
-	}
+	if(pipe(fd1)==-1) return 1;
 	int return_code2 = fork();
-	if(return_code2<0) printf("Child process 2 creation error\n");
+	if(return_code2<0) exit(1);
 	if(return_code2==0){
-		dup2(fd[0], STDIN_FILENO);
+		if(dup2(fd[0], STDIN_FILENO)<0) exit(1);
 		close(fd[0]);
 		dup2(fd1[1], STDOUT_FILENO);
 		close(fd1[1]);
@@ -42,16 +42,19 @@ int main(int argc, char *argv[])
 	close(fd1[1]);
 
 	int return_code3 = fork();
-	if(return_code3<0) printf("Child process 3 creation error\n");
+	if(return_code3<0) exit(1);
 	if(return_code3==0){
-		dup2(fd1[0], STDIN_FILENO);
+		if(dup2(fd1[0], STDIN_FILENO)<0) exit(1);
 		close(fd1[0]);
 		execlp(argv[3],argv[3],NULL);
 	}
 	close(fd1[0]);
 	int status1, status2, status3 = 0;
 	waitpid(return_code1, &status1, 0);
+	// printf("Child process 1 exits with code: %d\n", WEXITSTATUS(status1));
 	waitpid(return_code2, &status2, 0);
+	// printf("Child process 2 exits with code: %d\n", WEXITSTATUS(status2));
 	waitpid(return_code3, &status3, 0);
+	// printf("Child process 3 exits with code: %d\n", WEXITSTATUS(status3));
 	return 0;
 }
